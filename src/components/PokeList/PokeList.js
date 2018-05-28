e41ae1c45170a503bf36f14deede50b5048165a3
Fernando Guevara <luisfernandoguevara95@gmail.com>
2018-05-28 15:10:35 -0500

    Add sprites to pokedex

--- src/components/PokeList/PokeList.js
+++ src/components/PokeList/PokeList.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import './PokeList.css';
 
+import PokeCell from '../PokeCell';
+import { pokeClasses } from '../../pokeClasses';
+
 const PokeList = () => {
+    const cells = pokeClasses.map(pokeClass => {
+        return (
+            <PokeCell
+                key={pokeClass.id}
+                pokeClass={pokeClass}
+            />
+        );
+    });
+
     return (
         <section className="poke-list">
-
+            {cells}
         </section>
     )
 }
